b3cd02b2daaa6e2a03cde20a2bc9ad0defa21f6a
noahhendlish <nhendlis@tulane.edu>
2018-10-16 19:48:25 +0000

    added recomendder Model, View, & controller (not set up yet)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180927010844) do
+ActiveRecord::Schema.define(version: 20181016192226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(version: 20180927010844) do
     t.string   "credits"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "recommenders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
